6d1aa21b498a65269184baa877a3faf1f2034bec
MartenKlaesson <47351000+MartenKlaesson@users.noreply.github.com>
2019-04-11 14:45:15 +0200

    Update Pilon_sbatch.sh

--- Pilon_sbatch.sh
+++ Pilon_sbatch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 #SBATCH -A g2019003
 #SBATCH -p core
-#SBATCH -n 10
+#SBATCH -n 4
 #SBATCH -t 00:30:00
 #SBATCH -J Pilon_output_Marten_04_11
 #SBATCH --mail-type=ALL
@@ -12,4 +12,4 @@ module load Pilon/1.22
 # Your commands
 
 
-pilon --genome /home/martene/Genome_Analysis/Analysis/Genome_Assembly/Genome_Assembly.contigs.fasta --bam /home/martene/Genome_Analysis/Analysis/Scratch/BWA.sorted.bam --diploid --threads 10 --output Pilon_output 
+pilon --genome /home/martene/Genome_Analysis/Analysis/Genome_Assembly/Genome_Assembly.contigs.fasta --bam /home/martene/Genome_Analysis/Analysis/Scratch/BWA.sorted.bam --diploid --threads 4 --output /home/martene/Genome_Analysis/Analysis/Pilon_run/Pilon_output 
