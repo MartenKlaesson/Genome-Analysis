eeea6c5ebab0ed687ac970eab6ff1be0b03fab60
MartenKlaesson <47351000+MartenKlaesson@users.noreply.github.com>
2019-05-15 14:32:54 +0200

    Create Maker_secondstep.sh

---
+++ Maker_secondstep.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -l
+
+#SBATCH -A g2019003
+#SBATCH -p core
+#SBATCH -n 4
+#SBATCH -t 01:00:00
+#SBATCH -J Maker_secondstep_Marten_Klaesson
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user mklaesson91@gmail.com
+
+# Load modules
+module load bioinfo-tools
+module load maker
+
+
+# Commands
+
+cd /home/martene/Genome_Analysis/Analysis/maker/Pilon_output.maker.output 
+maker
