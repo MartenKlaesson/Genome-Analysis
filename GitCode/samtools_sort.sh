ed82d5d4214cedd22bccacf955f572d51832827a
MartenKlaesson <47351000+MartenKlaesson@users.noreply.github.com>
2019-05-30 14:17:03 +0200

    Sort genome using samtools

--- GitCode/samtools_sort.sh
+++ GitCode/samtools_sort.sh
@@ -5,3 +5,4 @@ do
 	output=$(basename $file)
 	samtools sort $file/accepted_hits.bam -o /home/martene/Genome_Analysis/Analysis/Bowtie2_Tophat/output_sorted/accepted_hits_sorted_2019-05-24_$output.bam
 done
+ 
